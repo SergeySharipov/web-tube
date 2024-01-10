5e84e6a708306c798f7c782f45f6ea7dd024106f
Serhii Sharipov <sharipov14@gmail.com>
2024-01-10 08:22:10 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+import colors from "tailwindcss/colors"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        secondary: {
+          DEFAULT: colors.neutral[100],
+          hover: colors.neutral[200],
+          border: colors.neutral[300],
+          text: colors.neutral[400],
+          dark: colors.neutral[700],
+          ["dark-hover"]: colors.neutral[800],
+        },
+      },
+    },
+  },
+  plugins: [],
+}
